bdb245cf5f922c7741d4c6c14e6824ca7235e099
SergeyKund <143610718+SergeyKund@users.noreply.github.com>
2023-09-14 14:11:15 +0300

    Add files via upload

--- NBU.py
+++ NBU.py
@@ -1,26 +1,37 @@
 import requests
 from datetime import date
-today = date.today()
-today_2 = today.strftime('%d-%m-%Y')
-today_str = str(today_2)
-today_str = today_str.replace('-', '.')
-print(f'На {today_str} курс такий:')
-today_str_file = f'На {today_str} курс такий:'
-try:
-    re = requests.request('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-    res = re.json()
-except:
-    print("error")
-else:
-    with open('val.txt', 'w') as file:
-        file.write(today_str_file + '\n')
-        for item in res:
-            if today_str in item.get('exchangedate', ''):
-                txt = item.get('txt')
-                rate = item.get('rate')
-                cc = item.get('cc')
-                content = f'{txt}({cc}) to UAH: {rate}'
-                file.write(content + '\n')
+class NBU:
+    def today_1(self):
+        today = date.today()
+        today_2 = today.strftime('%d-%m-%Y')
+        today_str = str(today_2)
+        today_str = today_str.replace('-', '.')
+        return today_str
 
+    def examination(self):
+        try:
+            re = requests.request('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+            res = re.json()
+        except:
+            print("error")
+        else:
+            with open('val.txt', 'w') as file:
+                file.write(f'На {self.today_1()} курс такий:' + '\n')
+                for item in res:
+                    if self.today_1() in item.get('exchangedate', ''):
+                        txt = item.get('txt')
+                        rate = item.get('rate')
+                        cc = item.get('cc')
+                        content = f'{txt}({cc}) to UAH: {rate}'
+                        file.write(content + '\n')
+                    elif not self.today_1() in item.get('exchangedate', ''):
+                        txt = item.get('txt')
+                        rate = item.get('rate')
+                        cc = item.get('cc')
+                        content = f'{txt}({cc}) to UAH: {rate}'
+        return file
+
+nbu = NBU()
+nbu.examination()
 
 
